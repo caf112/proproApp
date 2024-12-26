006276373d9585ae03b11bbf9d9a41cdd54c8664
caf112 <caf112.gt@gmail.com>
2024-12-26 21:16:26 +0900

    ディレクトリ構成を変更

---
+++ src/app/provider.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+// これにCognitoの情報を入れればいいのか？
+type AppProviderProps = {
+    // children: React.ReactNode;
+    // children?: React.ReactNode;
+};
+
+// export const AppProvider = ({children}: AppProviderProps) => {
+export const AppProvider = () => {
+
+
+  return (
+    <div>
+        {/* {children} */}
+        provider
+    </div>
+  )
+}
